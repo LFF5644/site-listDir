10bc9cabb0e177a2ab254614b2a423868471cf8d
LFF5644 <meerschweinchen5644@gamil.com>
2023-06-09 13:56:29 +0200

    use socket template

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const {
+	hook_effect,
+	hook_memo,
 	hook_model,
 	init,
 	node_dom,
@@ -8,11 +10,41 @@ const model={
 	init:()=>({
 		connected: false,
 	}),
+	assign:(state,object)=>({
+		...state,
+		...object,
+	}),
 };
 
 init(()=>{
 	const [state,actions]=hook_model(model);
+
+	const socket=hook_memo(()=> // connect & create socket
+		io({path:"/bind/socket/listDir"})
+	);
+
+	hook_effect(()=>{
+		// make vars global
+		window.actions=actions;
+		window.socket=socket;
+
+		// add socket event listeners
+		socket.on("connect",()=>{
+			actions.assign({connected: true});
+		});
+		socket.on("disconnect",()=>{
+			actions.assign({connected: false});
+		});
+	});
+	hook_effect(connected=>{
+		console.log("Socket has "+(connected?"connected":"disconnected")+"!");
+	},[state.connected]);
+
 	return[null,[
-		node_dom("h1[innerText=test]"),
+		node_dom("h1[innerText=Datei Explorer]",{
+			S:{
+				color: state.connected?"":"red",
+			}
+		}),
 	]];
 });
