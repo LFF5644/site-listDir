60d331f3ccfa394151b4c325eb529c736efc2bed
LFF5644 (LFF-Vivobook) <meerschweinchen5644@gmail.com>
2025-04-01 18:49:39 +0200

    make service back to work :P

--- server.service.js
+++ server.service.js
@@ -1,7 +1,6 @@
 const {randomBytes}=require("crypto");
-const directoryTools=require("directory-tools");
-const fs=require("fs");
 const socketIo=require("socket.io");
+const fs=require("fs");
 
 const fn=()=>{}; // a function that do nothing
 
@@ -11,24 +10,75 @@ const dynamic={
 };
 
 const socketListeners={
-	getFolderItems: (folder="",callback=fn)=>{
+	getFolderItems: async(folder="",callback=fn)=>{
 		folder=removeNotAllowed(folder);
 		if(!folder.endsWith("/")) folder+="/";
 		if(!folder.startsWith("/")) folder="/"+folder;
-
-		let items=directoryTools.getFolderItems("public/"+folder);
-		if(items) items=items.map(item=>({
+		const parentFolder="public";
+		//let items=directoryTools.getFolderItems("public/"+folder);
+		let items=[];
+		try{
+			items=await readdirAsync(parentFolder+folder);
+		}catch(e){
+			items=[];
+			log("readdir err: "+e.message);
+			callback(null);
+			return;
+		}
+		/*if(items) items=items.map(item=>({
 			id: randomBytes(8).toString("hex"),
 			name: item.name,
 			path: item.path?item.path:(folder+item.name),
 			type: item.type,
-		}));
-		console.log("items:",items);
+		}));*/
+		for(let index=0; index<items.length; index+=1){
+			const item=folder+items[index];
+			const name=item.split("/").pop();
+			let itemStats;
+			try{
+				itemStats=await statAsync(parentFolder+item);
+			}catch(e){
+				log("stat err: "+e.message);
+				items[index]=null;
+				continue;
+			}
+			const type=Object.entries({
+				blockDevice: itemStats.isBlockDevice(),
+				characterDevice: itemStats.isCharacterDevice(),
+				directory: itemStats.isDirectory(),
+				file: itemStats.isFile(),
+				socket: itemStats.isSocket(),
+				symbolicLink: itemStats.isSymbolicLink(),
+				other: true,
+			}).find(item=>item[1]===true)[0];
+			items[index]={
+				id: randomBytes(8).toString("hex"),
+				name,
+				path: item,
+				type,
+			};
+		}
+		items=items
+			.filter(item=>item!==null)
+			.sort((a,b)=>(a.type==="directory"&&b.type!=="directory")?-1:(a.type!=="directory"&&b.type==="directory")?1:a.name.localeCompare(b.name))
+		//console.log("items:",items);
 		callback(items);
 	},
 };
 
 // lib functions
+function readdirAsync(...args){return new Promise((resolve,reject)=>{
+	fs.readdir(...args,(error,files)=>{
+		if(error){reject(error); return;}
+		resolve(files);
+	});
+})}
+function statAsync(...args){return new Promise((resolve,reject)=>{
+	fs.stat(...args,(error,stat)=>{
+		if(error){reject(error); return;}
+		resolve(stat);
+	});
+})}
 function removeNotAllowed(string){
 	return(string
 		.split("..").join("")
